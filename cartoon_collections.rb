5d53a3dfdb970b2a3c960edb7781fc9d106a87df
favorable-vector-1743 <epzads@gmail.com>
2019-03-04 17:36:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,8 +24,10 @@ return new_array
   
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  
+  
 end
 
 def find_the_cheese# code an argument here
